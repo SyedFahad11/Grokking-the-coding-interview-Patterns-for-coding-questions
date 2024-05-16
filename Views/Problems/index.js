17ebce26be002706cf73c2ecbaf1fdce904bd1e5
SyedFahad11 <SYEDHU987@GMAIL.COM>
2024-05-17 00:01:51 +0530

    Created a function to do DOM manipulation and insert html

--- Views/Problems/index.js
+++ Views/Problems/index.js
@@ -1,3 +1,49 @@
+fetchDataFromFile('./problems.json')
+  .then(problems => {
+    console.log(problems[0]);
+    problemToHTML(problems[0])
+
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+function problemToHTML(problem) {
+
+  const patterns = problem.patterns.map(pattern => pattern.name).join(', ');
+
+
+  const problemContainer = document.createElement('div');
+  problemContainer.classList.add('problem-container');
+
+  problemContainer.innerHTML = `
+        <h2>${problem.title}</h2>
+        <p><strong>ID:</strong> ${problem.id}</p>
+        <p><strong>Difficulty:</strong> ${problem.difficulty}</p>
+        <p><strong>URL:</strong> <a href="${problem.url}">${problem.url}</a></p>
+        <p><strong>Status:</strong> ${problem.status}</p>
+
+        <button class="show-btn">Show Patterns & Tags</button>
+        <div class="hidden-section">
+            <p><strong>Tags:</strong> ${problem.tags.join(', ')}</p>
+            <p><strong>Patterns:</strong> ${patterns}</p>
+        </div>
+
+        <br />
+        <br />
+        <form id="problemForm" method="post" action="/updateStatus">
+            <label for="problemId">Problem ID:</label>
+            <input type="text" id="problemId" name="problemId" value="${problem.id}" required>
+            <button class="solved" type="submit">Solved</button>
+        </form>
+
+        <button class="generate-btn" onclick="newProblem()">Generate</button>
+    `;
+
+  document.body.appendChild(problemContainer);
+
+}
+
 
 async function fetchDataFromFile(filePath) {
   try {
@@ -17,15 +63,4 @@ async function fetchDataFromFile(filePath) {
     throw error;
   }
 }
-
-
-
-fetchDataFromFile('./problems.json')
-  .then(problems => {
-      console.log(problems[0]);
-
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
 
