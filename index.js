6a9dff6f46207041125f2a41f4365d838dc3a2bb
SyedFahad11 <SYEDHU987@GMAIL.COM>
2024-05-17 00:32:42 +0530

    Added functions to read and write into json

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors")
 const path = require('path');
 const bodyParser = require("body-parser")
+const fs = require('fs').promises;
+
 
 
 const app = express();
@@ -21,6 +23,30 @@ app.get("/", async function (req, res) {
 app.get('/solved', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/Solved/index.html'));
 })
+
+async function read(file) {
+  try {
+    const data = await fs.readFile(file, 'utf8');
+    const jsonArray = JSON.parse(data);
+    return jsonArray;
+  } catch (error) {
+    console.error('Error reading file:', error);
+    throw error;
+  }
+
+}
+
+async function write(filePath, data) {
+  try {
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
+    console.log('Data written to file successfully');
+  } catch (error) {
+    console.error('Error writing file:', error);
+    throw error;
+  }
+}
+
+
 
 
 
