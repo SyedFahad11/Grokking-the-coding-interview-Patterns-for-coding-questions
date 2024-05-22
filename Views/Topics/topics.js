acdd174c58a3e9d768dc086da865cd0a6bacba13
SyedFahad11 <SYEDHU987@GMAIL.COM>
2024-05-22 13:50:07 +0530

    Added topics table DOM code

--- Views/Topics/topics.js
+++ Views/Topics/topics.js
@@ -1,12 +1,46 @@
 
 fetchDataFromFile('./patterns.json')
-  .then(data => {
-    console.log(data);
-
+  .then(patternsArray => {
+    const solvedArray = Array(30).fill(0);
+    const totalArray = Array(30).fill(1);
+    addRowsToTable(patternsArray,solvedArray,totalArray);
   })
   .catch(error => {
     console.error('Error:', error);
   });
+
+  function addRowsToTable(patternsArray, solvedArray, totalArray) {
+
+    const table = document.getElementById('patternsTable');
+
+
+    if (!table) {
+      console.error('Table element not found!');
+      return;
+    }
+
+
+    for (let i = 0; i < patternsArray.length; i++) {
+
+      const row = document.createElement('tr');
+
+
+      const patternCell = document.createElement('td');
+      patternCell.textContent = patternsArray[i];
+      row.appendChild(patternCell);
+
+      const solvedCell = document.createElement('td');
+      solvedCell.textContent = solvedArray[i];
+      row.appendChild(solvedCell);
+
+      const totalCell = document.createElement('td');
+      totalCell.textContent = totalArray[i];
+      row.appendChild(totalCell);
+
+     
+      table.appendChild(row);
+    }
+  }
 
 async function fetchDataFromFile(filePath) {
   try {
