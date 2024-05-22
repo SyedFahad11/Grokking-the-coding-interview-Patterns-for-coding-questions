7154b595d780ebdd067b8ec3c19dfa668374a27f
SyedFahad11 <SYEDHU987@GMAIL.COM>
2024-05-22 12:51:23 +0530

    Added function to fetch data

--- Views/Solved/solved.js
+++ Views/Solved/solved.js
@@ -1,1 +1,22 @@
-alert("Hello")
+async function fetchDataFromFile(filePath) {
+  try {
+    const response = await fetch(filePath);
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
+fetchDataFromFile('./solved.json')
+  .then(data => {
+    console.log(data);
+
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
